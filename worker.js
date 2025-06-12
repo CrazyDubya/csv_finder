cdfe5902eea1bfcfc67b68eac29b1b1c31435888
Puppuccino <97849040+CrazyDubya@users.noreply.github.com>
2025-06-11 23:53:00 -0400

    Add virtual scrolling and worker

---
+++ worker.js
@@ -0,0 +1,31 @@
+importScripts('https://d3js.org/d3-dsv.v2.min.js');
+importScripts('https://cdnjs.cloudflare.com/ajax/libs/fuse.js/6.4.6/fuse.min.js');
+
+let data = [];
+let columns = [];
+let fuse = null;
+
+self.onmessage = function(e) {
+  const msg = e.data;
+  if (msg.type === 'parse') {
+    try {
+      data = d3.csvParse(msg.text);
+      columns = Object.keys(data[0] || {});
+      fuse = new Fuse(data, { keys: columns, threshold: 0.4, distance: 100 });
+      self.postMessage({ type: 'parsed', data, columns, total: data.length });
+    } catch (err) {
+      self.postMessage({ type: 'error', message: err.message });
+    }
+  } else if (msg.type === 'search') {
+    if (!fuse) return;
+    let results = msg.term ? fuse.search(msg.term).map(r => r.item) : data;
+    const filters = msg.filters || {};
+    Object.keys(filters).forEach(field => {
+      const val = filters[field];
+      if (val !== '') {
+        results = results.filter(row => String(row[field]).trim() === val);
+      }
+    });
+    self.postMessage({ type: 'results', results });
+  }
+};
